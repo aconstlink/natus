6c7fcc78f7880f53d5b786622bc51d73289b1df1
aconstlink <aconstlink@gmail.com>
2020-03-26 00:46:28 +0100

    [#2] more cmake install fup

--- cmake/functions/install_library.cmake
+++ cmake/functions/install_library.cmake
@@ -2,10 +2,11 @@
 function( install_library LIB_NAME TARGETS_NAME )
 
     install( TARGETS ${LIB_NAME} 
-        EXPORT TARGETS_NAME
+        EXPORT ${TARGETS_NAME}
         RUNTIME DESTINATION bin 
         LIBRARY DESTINATION lib
         ARCHIVE DESTINATION lib
     )
+    install( EXPORT ${TARGETS_NAME} DESTINATION "cmake" )
 
 endfunction()
