0eb414a5b6472057cf660f3de27b77c840d76e1a
aconstlink <aconstlink@gmail.com>
2020-05-05 17:41:09 +0200

    [#30] new binding point

--- gpu/shader/variable_binding.h
+++ gpu/shader/variable_binding.h
@@ -10,16 +10,18 @@ namespace natus
         enum class binding_point
         {
             undefined,
-            projection_matrix,
-            view_matrix,
-            camera_matrix,
-            camera_position
+            projection_matrix,  // mat4()
+            view_matrix,        // mat4()
+            camera_matrix,      // mat3()
+            camera_position,    // vec3( x, y, z )
+            viewport_dimension  // vec4( x, y, w, h )
         };
 
         namespace detail
         {
             static natus::std::string_t const __binding_point_names[] = { 
-                "undefined", "projection_matrix","view_matrix","camera_matrix","camera_position"
+                "undefined", "projection_matrix","view_matrix","camera_matrix","camera_position",
+                "viewport_dimension"
             } ;
         }
 
