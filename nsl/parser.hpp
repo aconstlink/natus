1c3bc16800edf50e1a6582576339be6f774af7b1
aconstlink <aconstlink@gmail.com>
2020-09-10 15:34:11 +0200

    [#143] - :construction: parser and some files

---
+++ nsl/parser.hpp
@@ -0,0 +1,152 @@
+
+
+#pragma once
+
+#include "typedefs.h"
+
+
+#include <natus/log/global.h>
+#include <natus/ntd/vector.hpp>
+
+namespace natus
+{
+    namespace nsl
+    {
+        class parser
+        {
+            natus_this_typedefs( parser ) ;
+
+        private:
+
+            natus::ntd::string_t _name ;
+
+        public:
+
+            typedef natus::ntd::vector< natus::ntd::string_t > statements_t ;
+
+        public:
+
+            parser( natus::ntd::string_cref_t name ) : _name( name ) {}
+            ~parser( void_t ) {}
+
+        public:
+
+
+            statements_t process( natus::ntd::string_cref_t file ) noexcept
+            {
+                statements_t statements ;
+
+                if( !this_t::check_curlies( file ) )
+                {
+                    natus::log::global_t::error("[parser] : curly braces not ok for [" + _name + "]" ) ;
+                    return statements_t() ;
+                }
+
+                size_t ooff = 0 ;
+                size_t opos = file.find_first_of( '{' ) ; // scope
+                while( opos != std::string::npos ) 
+                {
+                    natus::ntd::string_t line = this_t::clear_line( file.substr( ooff, opos-ooff ) ) ;
+
+                    statements.push_back( line ) ;
+
+                    if( line.find( "version" ) == 0 )
+                    {
+                        ooff = opos + 1 ;
+
+                        // take everything in until the next nsl closing } as content/statement
+                        // this need to be searched for because the code inside will have brackets too.
+                        {
+                            size_t count = 0 ;
+                            do {
+                                char_t const c = file[ opos++ ] ;
+                                count += ( c == '{' ? 1 : ( c == '}' ? -1 : 0 ) ) ;
+                            } while( count != 0 ) ;
+                        }
+                        statements.push_back( this_t::clear_line( file.substr( ooff, (--opos) - ooff ) ) ) ;
+                    }
+
+                    ooff = opos + 1;
+                    opos = file.find_first_of( '{', ooff ) ; // scope
+                    
+                    {
+                        size_t spos = file.find_first_of( ';', ooff ) ;
+                        while( spos < opos )
+                        {
+                            statements.push_back( this_t::clear_line( file.substr( ooff, ( spos + 1 ) - ooff ) ) ) ;
+                            ooff = spos + 1 ;
+                            spos = file.find_first_of( ';', ooff ) ;
+                        }
+                    }
+
+                    // check if closed is before next open - just drag the offset along
+                    {
+                        size_t cpos = file.find_first_of( '}', ooff ) ;
+                        if( cpos < opos ) 
+                        {
+                            ooff = cpos + 1 ;
+                            cpos = file.find_first_of( '}', ooff ) ;
+                            while( cpos < opos )
+                            {
+                                cpos = file.find_first_of( '}', cpos + 1 ) ;
+                                ooff = cpos + 1 ;
+                            }
+                        }
+                        
+                    }
+                }
+
+                return std::move( statements ) ;
+            }
+
+        private:
+
+            bool_t check_curlies( natus::ntd::string_cref_t file ) const noexcept
+            {
+                size_t count = 0 ;
+                for( auto const c : file )
+                {
+                    count = count + ( c == '{' ? 1 : (c == '}' ? -1 : 0 )  ) ;
+                }
+                return count == 0 ;
+            }
+
+            natus::ntd::string_t clear_line( natus::ntd::string_rref_t s ) const noexcept
+            {
+                for( size_t i=0; i<s.size(); ++i )
+                {
+                    if( s[ i ] == '\r' ) s[ i ] = ' ' ;
+                    if( s[ i ] == '\n' ) s[ i ] = ' ' ;
+                }
+                
+                // clear spaces before and after statement
+                {
+                    size_t const p0 = s.find_first_not_of( ' ' ) ;
+                    size_t const p1 = s.find_last_not_of( ' ' ) ;
+                    s = s.substr( p0, (p1 - p0) + 1 ) ;
+                }
+
+                // clear multi spaces
+                {
+                    size_t p0 = s.find_first_of( ' ' ) ;
+                    while( p0 != std::string::npos )
+                    {
+                        size_t const diff = s.find_first_not_of( ' ', p0 + 1 ) - p0 ;
+                        if( diff > 1 )
+                        {
+                            // +1 : just store one space back for pretty printing
+                            s = s.substr( 0, p0 + 1 ) + s.substr( p0 + diff ) ;
+                        }
+                        p0 = s.find_first_of( ' ', p0 + 1 ) ;
+                    }
+                }
+
+                return std::move( s ) ;
+            }
+
+            
+        };
+        natus_res_typedef( parser ) ;
+
+    }
+}
--- nsl/typedefs.h
+++ nsl/typedefs.h
@@ -1,7 +1,5 @@
 #pragma once
 
-#include "result.h"
-
 #include <natus/memory/global.h>
 #include <natus/memory/res.hpp>
 #include <natus/memory/macros.h>
@@ -18,16 +16,8 @@
 
 namespace natus
 {
-    namespace io
+    namespace nsl
     {
         using namespace natus::core::types ;
-
-        using memory = natus::memory::global ;
-        natus_typedefs( natus::ntd::filesystem::path, path ) ;
-
-        typedef ::std::function< void_t( char_cptr_t, size_t const, natus::io::result const ) > load_completion_funk_t ;
-        typedef ::std::function< void_t( natus::io::result const ) > store_completion_funk_t ;
-
-        typedef natus::core::void_ptr_t internal_item_ptr_t ;
     }
 }
