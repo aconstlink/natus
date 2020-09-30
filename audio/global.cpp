19b72f1c57dd5f338b68af96513d623e99d7c9e2
aconstlink <aconstlink@gmail.com>
2020-09-30 14:20:41 +0200

    [#93] - :construction: OpenAL along with wip audio layer

---
+++ audio/global.cpp
@@ -0,0 +1,30 @@
+
+#include "global.h"
+
+
+#include <natus/memory/global.h>
+
+using namespace natus::audio ;
+
+global_t::this_ptr_t global_t::_singleton = natus::memory::global_t::alloc( global_t(), "natus::audio::global" ) ;
+
+//***
+global::global( void_t )
+{
+}
+
+//***
+global::global( this_rref_t rhv ) 
+{
+}
+
+//***
+global::~global( void_t ) 
+{
+}
+
+//***
+global::this_ptr_t global::get( void_t ) 
+{
+    return _singleton ;
+}
---
+++ audio/global.h
@@ -0,0 +1,35 @@
+
+
+#pragma once
+
+#include "api.h"
+#include "typedefs.h"
+
+namespace natus
+{
+    namespace audio
+    {
+        class NATUS_AUDIO_API global
+        {
+            natus_this_typedefs( global ) ;
+
+        private: // singleton stuff
+
+            static this_ptr_t _singleton ;
+            static this_ptr_t get( void_t ) ;
+
+        private:
+
+        public:
+
+            global( void_t ) ;
+            global( this_cref_t ) = delete ;
+            global( this_rref_t ) ;
+            ~global( void_t ) ;
+
+        private:
+
+        };
+        natus_typedef( global ) ;
+    }
+}
