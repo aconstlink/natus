cb7324672dde5b4407d17cad3eb590adde2d70c8
aconstlink <aconstlink@gmail.com>
2021-03-20 02:57:37 +0100

    [#281] - :construction: task system

---
+++ concurrent/task/loose_thread_scheduler.hpp
@@ -0,0 +1,119 @@
+
+#pragma once
+
+#include "task.hpp"
+
+#include <natus/memory/global.h>
+
+#include <future>
+
+namespace natus
+{
+    namespace concurrent
+    {
+        class loose_thread_scheduler
+        {
+            natus_this_typedefs( loose_thread_scheduler ) ;
+
+        public:
+
+            natus_typedefs( natus::ntd::vector< natus::concurrent::task_res_t >, tasks ) ;
+
+        private:
+
+            struct shared_data
+            {
+                natus::concurrent::mutex_t mtx ;
+                this_ptr_t owner = nullptr ;
+
+                shared_data( void_t ) noexcept {}
+                shared_data( shared_data && rhv ) noexcept
+                {
+                    natus_move_member_ptr( owner, rhv ) ;
+                }
+            } ;
+            natus_typedef( shared_data ) ;
+            shared_data_ptr_t _sd = nullptr ;
+
+            natus::concurrent::mutex_t _mtx ;
+            tasks_t _tasks ;
+
+        public:
+
+            loose_thread_scheduler( void_t ) noexcept
+            {
+                shared_data sd ;
+                sd.owner = this ;
+                
+                _sd = natus::memory::global_t::alloc( std::move( sd ), 
+                    "[loose_thread_scheduler] : shared data" ) ;
+            }
+
+            loose_thread_scheduler( this_cref_t ) = delete ;
+            loose_thread_scheduler( this_rref_t rhv ) noexcept
+            {
+                {
+                    natus::concurrent::lock_t lk(rhv._sd->mtx) ;
+                    rhv._sd->owner = this ;
+                }
+                natus_move_member_ptr( _sd, rhv ) ;
+                _tasks = std::move( rhv._tasks ) ;
+            }
+
+            ~loose_thread_scheduler( void_t ) noexcept
+            {
+                natus::memory::global_t::dealloc( _sd ) ;
+            }
+
+        private:
+
+            void_t execute( task_res_t t ) noexcept
+            {
+                if( natus::concurrent::task::scheduler_accessor::will_execute( t ) ) 
+                {
+                    auto f = std::async( std::launch::async, [=]( void_t )
+                    {
+                        natus::concurrent::task::scheduler_accessor::execute( t ) ;
+
+                        tasks_t tasks ;
+                        natus::concurrent::task::scheduler_accessor::schedule( t, tasks ) ;
+
+                        _sd->owner->schedule( std::move( tasks ) ) ;
+                    } ) ;
+                }
+            }
+
+        public:
+
+            void_t schedule( task_res_t t ) noexcept
+            {
+                natus::concurrent::lock_t lk( _mtx ) ;
+                 _tasks.emplace_back( std::move( t ) ) ;
+            }
+
+            void_t schedule( tasks_rref_t tasks ) noexcept
+            {
+                natus::concurrent::lock_t lk( _mtx ) ;
+                for( auto t : tasks )
+                {
+                    _tasks.emplace_back( std::move( t ) ) ;
+                }
+            }
+
+            void_t update( void_t ) noexcept
+            {
+                tasks_t tasks ;
+                {
+                    natus::concurrent::lock_t lk( _mtx ) ;
+                    tasks = std::move( _tasks ) ;
+                }
+
+                for( auto & t : tasks )
+                {
+                    this_t::execute( t ) ;
+                }
+            }
+
+        };
+    }
+}
---
+++ concurrent/task/task.hpp
@@ -0,0 +1,163 @@
+
+#pragma once
+
+#include "../api.h"
+#include "../typedefs.h"
+
+#include <natus/ntd/vector.hpp>
+
+#include <atomic>
+
+namespace natus
+{
+    namespace concurrent
+    {
+        class task
+        {
+            natus_this_typedefs( task ) ;
+            typedef natus::memory::res< task > this_res_t ;
+
+            friend class task ;
+
+        public:
+
+            typedef std::function< void_t ( this_res_t ) > task_funk_t ;
+
+        private: // user execution funktion
+
+            task_funk_t _funk ;
+
+        private: // refs
+
+            natus::concurrent::mutex_t _incomings_mtx ;
+            size_t _incomings = 0 ;
+
+            bool_t dec_incoming( void_t ) noexcept 
+            {
+                natus::concurrent::lock_guard_t lk( _incomings_mtx ) ;
+                if( --_incomings == size_t(-1) ) _incomings = 0 ;
+                return _incomings == 0 ;
+            }
+
+            void_t inc_incoming( void_t ) noexcept
+            {
+                natus::concurrent::lock_guard_t lk( _incomings_mtx ) ;
+                ++_incomings ;
+            }
+
+        private: //
+
+            natus::ntd::vector< this_res_t > _inbetweens ;
+            natus::ntd::vector< this_res_t > _thens ;
+
+        public:
+
+            task( void_t ) noexcept
+            {
+                _funk = [=]( this_res_t ){} ;
+            }
+
+            task( task_funk_t f ) noexcept
+            {
+                _funk = f ;
+            }
+
+            task( this_cref_t ) = delete ;
+            task( this_rref_t rhv ) noexcept
+            {
+                _funk = std::move( rhv._funk ) ;
+                _inbetweens = std::move( rhv._inbetweens ) ;
+                _thens = std::move( rhv._thens ) ;
+                _incomings = rhv._incomings ;
+            }
+
+            this_ref_t operator = ( this_rref_t rhv ) noexcept
+            {
+                _funk = std::move( rhv._funk ) ;
+                _inbetweens = std::move( rhv._inbetweens ) ;
+                _thens = std::move( rhv._thens ) ;
+                _incomings = rhv._incomings ;
+                return *this ;
+            }
+
+        public: // this is what the user should use only
+
+            this_res_t in_between( this_res_t other ) noexcept
+            {
+                _inbetweens.emplace_back( other ) ;
+                other->inc_incoming() ;
+                return other ;
+            }
+
+            this_res_t then( this_res_t other ) noexcept
+            {
+                _thens.emplace_back( other ) ;
+                other->inc_incoming() ;
+                return other ;
+            }
+
+        public:
+
+            struct scheduler_accessor
+            {
+                static void_t schedule( this_res_t t, natus::ntd::vector< this_res_t > & tasks ) noexcept
+                {
+                    t->schedule( tasks ) ;
+                }
+
+                static bool_t will_execute( this_res_t t ) noexcept
+                {
+                    return t->will_execute() ;
+                }
+
+                static bool_t execute( this_res_t t ) noexcept 
+                {
+                    return t->execute( t ) ;
+                }
+            };
+            friend struct scheduler_accessor ;
+
+        private:
+
+            void_t schedule( natus::ntd::vector< this_res_t > & tasks ) noexcept
+            {
+                tasks.reserve( tasks.capacity() + _inbetweens.size() + _thens.size() ) ;
+
+                if( _inbetweens.size() == 0 )
+                {
+                    for( auto & t : _thens ) tasks.emplace_back( t ) ;
+                    return ;
+                }
+
+                for( auto & c : _inbetweens )
+                {
+                    for( auto & t : _thens ) 
+                    {
+                        c->then( t ) ;
+
+                        // t is not scheduled yet
+                        t->dec_incoming() ; 
+                    }
+                    tasks.emplace_back( c ) ;
+                }
+            }
+
+            bool_t will_execute( void_t ) noexcept
+            {
+                return this_t::dec_incoming() ;
+            }
+
+            // the schedule will pass the resource handle
+            // of this task to this function so the
+            // executing function could add futher tasks
+            bool_t execute( this_res_t this_res ) noexcept 
+            {
+                if( _incomings != 0 ) return false ;
+                
+                 _funk( this_res ) ;
+                 return true ;
+            }
+        };
+        natus_res_typedef( task ) ;
+    }
+}
