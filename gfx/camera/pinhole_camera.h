265a3efa589218d902ee8c653f97115588c92864
aconstlink <aconstlink@gmail.com>
2022-01-05 11:58:15 +0100

    getting position from camera

--- gfx/camera/pinhole_camera.h
+++ gfx/camera/pinhole_camera.h
@@ -17,7 +17,7 @@ namespace natus
 
         public:
 
-            pinhole_camera( void_t ) 
+            pinhole_camera( void_t ) noexcept
             {
                 _lens = natus::gfx::pinhole_lens_res_t( natus::gfx::pinhole_lens_t() ) ;
                 _camera = natus::gfx::generic_camera_res_t( natus::gfx::generic_camera_t() ) ;
@@ -25,17 +25,17 @@ namespace natus
                 _camera->add_lens( _lens ) ;
             }
 
-            pinhole_camera( this_rref_t rhv )
+            pinhole_camera( this_rref_t rhv ) noexcept
             {
                 *this = ::std::move( rhv ) ;
             }
 
             pinhole_camera( this_cref_t rhv ) = delete ;
 
-            ~pinhole_camera( void_t )
+            ~pinhole_camera( void_t ) noexcept
             {}
 
-            this_ref_t operator = ( this_rref_t rhv ) 
+            this_ref_t operator = ( this_rref_t rhv ) noexcept
             {
                 _lens = ::std::move( rhv._lens ) ;
                 _camera = ::std::move( rhv._camera ) ;
@@ -45,20 +45,20 @@ namespace natus
         public:
 
             this_ref_t orthographic( float_t const w, float_t const h,
-                float_t const n, float_t const f  )
+                float_t const n, float_t const f  ) noexcept
             {
                 _lens->make_orthographic( w, h, n, f ) ;
                 return *this ;
             }
 
             this_ref_t perspective_fov( float_t const fov, float_t const aspect,
-                float_t const n, float_t const f  )
+                float_t const n, float_t const f  ) noexcept
             {
                 _lens->make_perspective_fov( fov, aspect, n, f ) ;
                 return *this ;
             }
 
-            this_ref_t translate_by( natus::math::vec3f_cref_t d )
+            this_ref_t translate_by( natus::math::vec3f_cref_t d ) noexcept
             {
                 natus::math::m3d::trafof_t t ;
                 t.translate_fr( d ) ;
@@ -66,8 +66,13 @@ namespace natus
                 return *this ;
             }
 
+            natus::math::vec3f_t get_position( void_t ) const noexcept
+            {
+                return _camera->get_transformation().get_translation() ;
+            }
+
             this_ref_t look_at( natus::math::vec3f_cref_t pos,
-                natus::math::vec3f_cref_t up, natus::math::vec3f_cref_t at ) 
+                natus::math::vec3f_cref_t up, natus::math::vec3f_cref_t at ) noexcept
             {
                 _camera->for_each_lens( [&] ( icamera_ref_t, lens_res_t& l ) 
                 { 
